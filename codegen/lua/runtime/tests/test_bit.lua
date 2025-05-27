b09693962c6e6cf6d810efee5d3997fbb51756f9
kajacx <kajacx@gmail.com>
2025-05-27 13:14:16 +0200

    Additional extract_bytes tests

--- codegen/lua/runtime/tests/test_bit.lua
+++ codegen/lua/runtime/tests/test_bit.lua
@@ -13,14 +13,14 @@ local function expect_error(callback, descr)
     end
 end
 
-local value = 5 * (256 ^ 0) + 8 * (256 ^ 1) + 240 * (256 ^ 2) + 50 * (256 ^ 3)
+local value = 5 * (256 ^ 0) + 8 * (256 ^ 1) + 240 * (256 ^ 2) + 11 * (256 ^ 3)
 assert_equals(bit.extract_bytes(value, 0, 1), 5)
 assert_equals(bit.extract_bytes(value, 1, 1), 8)
 assert_equals(bit.extract_bytes(value, 2, 1), 240)
-assert_equals(bit.extract_bytes(value, 3, 1), 50)
+assert_equals(bit.extract_bytes(value, 3, 1), 11)
 assert_equals(bit.extract_bytes(value, 0, 2), 5 + 8 * 256)
 assert_equals(bit.extract_bytes(value, 1, 2), 8 + 240 * 256)
-assert_equals(bit.extract_bytes(value, 2, 2), 240 + 50 * 256)
+assert_equals(bit.extract_bytes(value, 2, 2), 240 + 11 * 256)
 assert_equals(bit.extract_bytes(value, 0, 4), value)
 
 local new_value
@@ -28,18 +28,41 @@ local new_value
 -- single byte at start
 new_value = bit.set_bytes(value, 0, 1, 55)
 assert_equals(bit.extract_bytes(new_value, 0, 1), 55)
-assert_equals(bit.extract_bytes(new_value, 1, 3), 8 * (256 ^ 0) + 240 * (256 ^ 1) + 50 * (256 ^ 2))
+assert_equals(bit.extract_bytes(new_value, 1, 3), 8 * (256 ^ 0) + 240 * (256 ^ 1) + 11 * (256 ^ 2))
 
 -- single byte in the middle
 new_value = bit.set_bytes(value, 1, 1, 88)
 assert_equals(bit.extract_bytes(new_value, 0, 1), 5)
 assert_equals(bit.extract_bytes(new_value, 1, 1), 88)
-assert_equals(bit.extract_bytes(new_value, 2, 2), 240 + 50 * 256)
+assert_equals(bit.extract_bytes(new_value, 2, 2), 240 + 11 * 256)
 
 -- single byte at end
-new_value = bit.set_bytes(value, 3, 1, 250)
+new_value = bit.set_bytes(value, 3, 1, 111)
 assert_equals(bit.extract_bytes(new_value, 0, 3), 5 * (256 ^ 0) + 8 * (256 ^ 1) + 240 * (256 ^ 2))
-assert_equals(bit.extract_bytes(new_value, 3, 1), 250)
+assert_equals(bit.extract_bytes(new_value, 3, 1), 111)
+
+
+-- double byte at start
+new_value = bit.set_bytes(value, 0, 2, 55 + 88 * 256)
+assert_equals(bit.extract_bytes(new_value, 0, 2), 55 + 88 * 256)
+assert_equals(bit.extract_bytes(new_value, 2, 2), 240 + 11 * 256)
+
+-- double byte in middle
+new_value = bit.set_bytes(value, 1, 2, 88 + 250 * 256)
+assert_equals(bit.extract_bytes(new_value, 0, 1), 5)
+assert_equals(bit.extract_bytes(new_value, 1, 2), 88 + 250 * 256)
+assert_equals(bit.extract_bytes(new_value, 3, 1), 11)
+
+-- double byte at end
+new_value = bit.set_bytes(value, 2, 2, 250 + 111 * 256)
+assert_equals(bit.extract_bytes(new_value, 0, 2), 5 + 8 * 256)
+assert_equals(bit.extract_bytes(new_value, 2, 2), 250 + 111 * 256)
+
+
+-- full set_bytes
+local new_value_num = 55 * (256 ^ 0) + 88 * (256 ^ 1) + 250 * (256 ^ 2) + 111 * (256 ^ 3)
+new_value = bit.set_bytes(value, 0, 4, new_value_num)
+assert_equals(bit.extract_bytes(new_value, 0, 4), new_value_num)
 
 expect_error(function() bit.extract_bytes(value, -5, 2) end, "start byte negative")
 expect_error(function() bit.extract_bytes(value, 5, 2) end, "start byte too large")
